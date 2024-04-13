7b543a316953fd58a5f1d8ed01b6315acaa591a4
GABOBB <gabbol2002@gmail.com>
2024-04-13 12:36:50 -0600

    se reimplementan las ventanas de Main menu y log in v0.2.0

--- MMenu.py
+++ MMenu.py
@@ -5,46 +5,22 @@ import sys
 import os
 
 
-class MMenu:
+class Menu_wndw(tk.Tk):
 
     def __init__(self):
-        self.__Menu=None#refers to the window (Tk)
-        self.canvas = None
-        
-    def cargarImagen(self,nombre):
-        ruta = os.path.join('auxiliar',nombre)
-        imagen = PhotoImage(file=ruta)
-        return imagen
-    
-  
-    def Sln(self):
-        L = LGIN.login(self.canvas)
-        Log_C = L.get_C()
-        self.canvas.destroy()
-        self.canvas = Log_C
-    
-    def Sln2(self):
-        L2 = LGIN.login()
-        Log2_C = L2.get_C()
-        self.canvas2 = Log2_C
-        self.canvas.place(x=500,y=10)    
-    
+        super().__init__()
+        self.title('GalaTec')
+        self.geometry('711x400')
+        self.resizable(width=NO,height=NO)
+        self.wm_attributes('-topmost',1)
 
-    def window(self):
-        print(3)
-        self.__Menu = Tk()
-        self.__Menu.geometry('711x400')
-        self.__Menu.title('MAIN_MENU')
-        self.__Menu.resizable(width=NO,height=NO)
-        self.__Menu.wm_attributes('-topmost',1)
-
-        self.canvas = tk.Canvas(self.__Menu, bg="black", width=711, height=400)
+        self.canvas = tk.Canvas(self, bg="black", width=711, height=400)
         self.canvas.pack()
         
-        SingleP_B= Button(self.canvas, text='Single Player',bg='green1',fg='black',command=lambda: self.Sln())
+        SingleP_B = Button(self.canvas, text='Single Player',bg='green1',fg='black',command=lambda: self.S_ln())
         SingleP_B.place(x=20,y=180)
 
-        MultiP_B = Button(self.canvas, text= ' Multiplayer ',bg='green1',fg='black',command=lambda: self.Sln2())
+        MultiP_B = Button(self.canvas, text= ' Multiplayer ',bg='green1',fg='black',command=lambda: self.MP_ln())
         MultiP_B.place(x=20,y=220)
         
         Sttngs_B = Button(self.canvas, text='Game Settings', bg='green1',fg='black')
@@ -53,11 +29,24 @@ class MMenu:
         PodioG_B = Button(self.canvas, text='Podio', bg='green1', fg='black')
         PodioG_B.place(x=20,y=300)
         
-        ExitGm_B = Button(self.canvas, text='Exit Game', bg = 'green1', fg= 'black', command=lambda: self.__Menu.destroy())
+        ExitGm_B = Button(self.canvas, text='Exit Game', bg = 'green1', fg= 'black', command=lambda: self.destroy())
         ExitGm_B.place(x=20,y=340)
 
-        self.__Menu.mainloop()
+        self.mainloop()
+        
+        
+    def cargarImagen(self,nombre):
+        ruta = os.path.join('auxiliar',nombre)
+        imagen = PhotoImage(file=ruta)
+        return imagen
+    
+  
+    def S_ln(self):
+        self.destroy()
+        LGIN.login_wndw()
+    
+    def MP_ln(self):
+        pass
+    
+Menu_wndw()
 
-
-M=MMenu()
-M.window()
--- logIn.py
+++ logIn.py
@@ -4,35 +4,18 @@ import pygame
 import sys
 import os
 
-class login:
 
-    def __init__(self,C_A):
-        self.canvas=None
-        self.load()
-        self.C_A = C_A
+class login_wndw(tk.Tk):
+    
+    def __init__(self):
+        super().__init__()
+        self.title("Log In")
+        self.geometry('250x300')
+        self.title('Log In')
+        self.resizable(width=NO,height=NO)
         
-        
-
-    
-    
-    def begin(self,X,y):
-        print(X,y)
-        # se verificarian las credenciales
-    
-    def get_C(self):
-        return self.canvas
-    
-    def exit(self):
-        self.canvas.destroy
-        #self.canvas = self.C_A
-    
-
-    def load(self):
-       
-        self.canvas = tk.Canvas( bg="black", width=250, height=300)
+        self.canvas = tk.Canvas(self, bg="black", width=250, height=300)
         self.canvas.pack()
-        
-        
         
         UserL=Label(self.canvas, text='Username:',bg='green2',fg='Black')
         UserL.place(x=15,y=50)
@@ -46,8 +29,24 @@ class login:
         PwrdE=Entry(self.canvas,bg='black',fg='green2')
         PwrdE.place(x=10,y=180)
 
-        LoginB = Button(self.canvas, text= 'LogIn',bg='green1',fg='black',command=lambda: self.begin(UserE.get(),PwrdE.get()))
+        LoginB = Button(self.canvas, text= 'LogIn',bg='green1',fg='black',command=lambda: self.try_logIn())
         LoginB.place(x=20,y=240)
         
         cancel = Button(self.canvas,text='Exit',bg='green1',command=lambda: self.exit())
         cancel.place(x=80,y=240)
+        
+        Rgstr = Button(self.canvas, text='Dont have an account yet?',bg='black',fg='green1',bd=0, command=lambda: self.Sing_Up())
+        Rgstr.place(x=10,y=200)
+        
+        self.mainloop()
+        
+    def exit(self):
+        print('exit')
+        
+    def try_logIn(self):
+        print('Log In')
+    
+    def Sing_Up(self):
+        print('sing_Up')
+
+#login_wndw()
